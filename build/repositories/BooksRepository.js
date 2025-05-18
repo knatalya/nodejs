f43c76f1eb94e033fef981847fda913d35769c39
Natalya Kaukenova <49254621+knatalya@users.noreply.github.com>
2025-05-18 16:50:12 +0300

    Delete build directory

--- build/repositories/BooksRepository.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.BooksRepository = void 0;
-class BooksRepository {
-}
-exports.BooksRepository = BooksRepository;
